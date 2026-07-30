de1731b28a204a02281bf31519327085e88fb5a6
Asha <india.lumos@gmail.com>
2026-07-30 10:58:33 +0530

    accessibility fix

--- static/melpalani-org/custom.js
+++ static/melpalani-org/custom.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-        AOS.init();
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init();
+});
 
-    
 // $(document).ready(function() {
 //     var $slider = $('.custom-slider');
 //     var totalSlides = $slider.find('.custom-box').length;
@@ -50,47 +49,61 @@ document.addEventListener("DOMContentLoaded", function() {
 //     $('.custom-slider-number').text(currentIndex + '/' + totalSlides);
 // });
 
-
-var $slider = $(".client-slider")
-  , $paginationNumber = $(".pagination-number");
+var $slider = $(".client-slider"),
+  $paginationNumber = $(".pagination-number");
 function padNumber(e) {
-    return e < 10 ? "0" + e : e
+  return e < 10 ? "0" + e : e;
 }
 $slider.slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: !0,
-    autoplaySpeed: 3000,
-    arrows: !0,
-    dots: !1,
-    adaptiveHeight: !0,
-    responsive: [{
-        breakpoint: 1121,
-        settings: {
-            slidesToShow: 3,
-            autoplay: !0,
-            adaptiveHeight: !0
-        }
-    }, {
-        breakpoint: 767,
-        settings: {
-            slidesToShow: 1,
-            autoplay: !0,
-            adaptiveHeight: !0
-        }
-    }, {
-        breakpoint: 991,
-        settings: {
-            slidesToShow: 2,
-            autoplay: !0,
-            adaptiveHeight: !0
-        }
-    }]
-}),
-$slider.on("init reInit afterChange", function(e, t, i) {
-    var o = t.currentSlide + 1
-      , n = t.slideCount;
-    $paginationNumber.text(padNumber(o) + "/" + padNumber(n))
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: !0,
+  autoplaySpeed: 3000,
+  arrows: !0,
+  dots: !1,
+  adaptiveHeight: !0,
+  responsive: [
+    {
+      breakpoint: 1121,
+      settings: {
+        slidesToShow: 3,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+  ],
+});
+
+// Set accessible names on slick-track elements for ARIA compliance
+$(".client-slider").each(function () {
+  var $track = $(this).find(".slick-track");
+  if ($(this).closest("#temples").length > 0) {
+    $track.attr("aria-label", "Temples");
+  } else {
+    $track.attr("aria-label", "Events");
+  }
+});
+
+$slider.on("init reInit afterChange", function (e, t, i) {
+  var o = t.currentSlide + 1,
+    n = t.slideCount;
+  $paginationNumber.text(padNumber(o) + "/" + padNumber(n));
 });
 
 // $(document).ready(function() {
@@ -143,6 +156,11 @@ $slider.on("init reInit afterChange", function(e, t, i) {
 //     $('.slick-carousel-number').text(currentIndexCarousel + '/' + totalSlidesCarousel);
 // });
 
-$(document).ready(function(){       
-   $('#myModal').modal('show');
-    }); 
+$(document).ready(function () {
+  // Add accessible names to icon-only and carousel links for compliance
+  $("a.nav-link:has(i.fa-home)").attr("aria-label", "Home");
+  $(".carousel-control-prev").attr("aria-label", "Previous");
+  $(".carousel-control-next").attr("aria-label", "Next");
+
+  $("#myModal").modal("show");
+});
