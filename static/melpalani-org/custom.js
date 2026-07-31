f188554d98105c1239e74298cd324ef549be4d5c
Asha <india.lumos@gmail.com>
2026-07-31 12:47:09 +0530

    temples section

--- static/melpalani-org/custom.js
+++ static/melpalani-org/custom.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
 var $slider = $(".client-slider"),
   $paginationNumber = $(".pagination-number");
 
+var $templesSlider = $(".temples-slider"),
+  $templesPaginationNumber = $(".temples-pagination-number");
+
 function padNumber(e) {
   return e < 10 ? "0" + e : e;
 }
@@ -15,6 +18,12 @@ $slider.on("init reInit afterChange", function (e, t, i) {
   var o = t.currentSlide + 1,
     n = t.slideCount;
   $paginationNumber.text(padNumber(o) + "/" + padNumber(n));
+});
+
+$templesSlider.on("init reInit afterChange", function (e, t, i) {
+  var o = t.currentSlide + 1,
+    n = t.slideCount;
+  $templesPaginationNumber.text(padNumber(o) + "/" + padNumber(n));
 });
 
 /* ----------------------------- Slick Carousel Slider Initialization ----------------------------- */
@@ -54,14 +63,51 @@ $slider.slick({
   ],
 });
 
+$templesSlider.slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: !0,
+  autoplaySpeed: 3000,
+  arrows: !0,
+  dots: !1,
+  adaptiveHeight: !0,
+  responsive: [
+    {
+      breakpoint: 1121,
+      settings: {
+        slidesToShow: 3,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        autoplay: !0,
+        adaptiveHeight: !0,
+      },
+    },
+  ],
+});
+
 /* ----------------------------- ARIA Accessibility Compliances ----------------------------- */
 $(".client-slider").each(function () {
   var $track = $(this).find(".slick-track");
-  if ($(this).closest("#temples").length > 0) {
-    $track.attr("aria-label", "Temples");
-  } else {
-    $track.attr("aria-label", "Events");
-  }
+  $track.attr("aria-label", "Events");
+});
+
+$(".temples-slider").each(function () {
+  var $track = $(this).find(".slick-track");
+  $track.attr("aria-label", "Temples");
 });
 
 /* ----------------------------- Page Navigation & Google Calendar Modals ----------------------------- */
